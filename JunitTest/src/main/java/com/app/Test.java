6404478dc7ece0efb501216fa8464d0875900c41
narine9126 <narine9126@gmail.com>
2021-12-13 00:48:42 +0530

    statement modified

--- JunitTest/src/main/java/com/app/Test.java
+++ JunitTest/src/main/java/com/app/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello github");
-		System.out.println("new statement");
+		System.out.println("new statement1");
 	}
 
 }
